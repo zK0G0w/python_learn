5dd8d8d5c6360bff41ffcb382752fa27a98112ba
zengwen <zk0g0w@gmail.com>
2023-09-08 14:38:43 +0800

    no message

---
+++ 01_初识Python/01_变量.py
@@ -0,0 +1,35 @@
+a = 1
+b = 2
+c = 3
+c_1 = 4
+str = 'I\'m \"OK\"!'
+f1 = 1.23e10
+f2 = 1.2e-5
+_1 = 10000000
+_2 = 10_00_00_00
+age = 22
+
+if not (age >= 18):
+    print('adult')
+else:
+    print('teenager')
+print(a)
+print(b)
+print(c)
+print(c_1)
+print(str)
+print(f1)
+print(f2)
+print(_1)
+print(_2)
+print(r"\\\\n\\\\\\")
+print("\\\n\\\\\\")
+print(r'\\\\n\\\\\\')
+print('''line1
+line2
+line3''')
+print(r'''hello,\n
+world''')
+print(True and True)
+
+
